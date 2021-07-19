8d0fb309291ea2354f297ef1346cdabf00a9d8c3
Justin Wheeler <justin.wheeler@gfs.com>
2021-07-19 10:09:57 -0400

    Fixed Failing jUnit Tests, Updated .gitignore for Eclipse and IntelliJ iml

--- src/test/java/com/bravo/user/config/AppConfigTest.java
+++ src/test/java/com/bravo/user/config/AppConfigTest.java
@@ -7,12 +7,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ContextConfiguration(classes = {App.class})
 @ExtendWith(SpringExtension.class)
+@SpringBootTest
 @TestPropertySource(properties = {
     "server.port=9090",
 })
@@ -23,20 +25,24 @@ public class AppConfigTest {
 
   @Test
   public void serverPort(){
-    final int expect = 9090;
     final int actual = appConfig.serverPort();
+    final int expect = 9090;
     Assertions.assertEquals(expect, actual);
   }
 
   @Test
   public void objectMapperBuilder(){
     final ObjectMapper objectMapper = appConfig.objectMapperBuilder().build();
-    Assertions.assertEquals("object mapper clazz", objectMapper.getClass().getName());
+    final String actual = objectMapper.getClass().getName();
+    final String expect = "com.fasterxml.jackson.databind.ObjectMapper";
+    Assertions.assertEquals(expect, actual);
   }
 
   @Test
   public void mapperFacade(){
     final MapperFacade mapperFacade = appConfig.mapperFacade();
-    Assertions.assertEquals("mapper facade clazz", mapperFacade.getClass().getName());
+    final String actual = mapperFacade.getClass().getName();
+    final String expect = "ma.glasnost.orika.impl.MapperFacadeImpl";
+    Assertions.assertEquals(expect, actual);
   }
 }
