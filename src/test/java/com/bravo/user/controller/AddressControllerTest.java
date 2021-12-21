7bd660d9ca67fc52ae24c640519561107fb2b23c
kkruegs101 <kyle+bravo@krueger.pro>
2021-12-21 01:06:32 -0500

    implement test cases for new Address classes

--- src/test/java/com/bravo/user/controller/AddressControllerTest.java
+++ src/test/java/com/bravo/user/controller/AddressControllerTest.java
@@ -1,14 +1,85 @@
 package com.bravo.user.controller;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
-public class AddressControllerTest {
+import com.bravo.user.App;
+import com.bravo.user.model.dto.AddressDto;
+import com.bravo.user.service.AddressService;
+
+@ContextConfiguration(classes = { App.class })
+@ExtendWith(SpringExtension.class)
+@SpringBootTest()
+@AutoConfigureMockMvc
+class AddressControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private AddressService addressService;
+
+	private List<AddressDto> addresses;
+
+	@BeforeEach
+	public void beforeEach() {
+		final List<Integer> ids = IntStream.range(1, 10).boxed().collect(Collectors.toList());
+
+		this.addresses = ids.stream().map(id -> createAddressDto(Integer.toString(id)))
+				.collect(Collectors.toList());
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	void getRetrieveByUserId() throws Exception {
+		String userId = "123a-456b";
+
+		when(addressService.retrieveByUserId(anyString())).thenReturn(addresses);
+
+		final ResultActions result = this.mockMvc.perform(get("/address/retrieve/".concat(userId)))
+				.andExpect(status().isOk());
+
+		for (int i = 0; i < addresses.size(); i++) {
+			result.andExpect(
+					jsonPath(String.format("$[%d].id", i)).value(addresses.get(i).getId()));
+		}
+
+		verify(addressService).retrieveByUserId(userId);
+	}
+
+	@Test
+	void getRetrieveByUserId_Space() throws Exception {
+		this.mockMvc.perform(get("/address/retrieve/ /")).andExpect(status().isBadRequest());
+	}
+
+	@Test
+	void getRetrieveByUserId_Missing() throws Exception {
+		this.mockMvc.perform(get("/address/retrieve")).andExpect(status().isNotFound());
+	}
+
+	private AddressDto createAddressDto(final String id) {
+		final AddressDto address = new AddressDto();
+		address.setId(id);
+		return address;
 	}
 
 }
--- src/test/java/com/bravo/user/service/AddressServiceTest.java
+++ src/test/java/com/bravo/user/service/AddressServiceTest.java
@@ -1,14 +1,79 @@
 package com.bravo.user.service;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-public class AddressServiceTest {
+import com.bravo.user.App;
+import com.bravo.user.dao.model.Address;
+import com.bravo.user.dao.model.mapper.ResourceMapper;
+import com.bravo.user.dao.repository.AddressRepository;
+import com.bravo.user.model.dto.AddressDto;
+
+@ContextConfiguration(classes = { App.class })
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class AddressServiceTest {
+
+	@Autowired
+	private AddressService addressService;
+
+	@MockBean
+	private ResourceMapper resourceMapper;
+
+	@MockBean
+	private AddressRepository addressRepository;
+
+	private List<AddressDto> dtoAddresses;
+
+	@BeforeEach
+	public void beforeEach() {
+		final List<Integer> ids = IntStream.range(1, 10).boxed().collect(Collectors.toList());
+
+		final List<Address> daoAddresses = ids.stream()
+				.map(id -> createAddress(Integer.toString(id))).collect(Collectors.toList());
+
+		when(addressRepository.findByUserId(anyString())).thenReturn(daoAddresses);
+
+		this.dtoAddresses = ids.stream().map(id -> createAddressDto(Integer.toString(id)))
+				.collect(Collectors.toList());
+
+		when(resourceMapper.convertAddresses(daoAddresses)).thenReturn(dtoAddresses);
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	void retrieveByUserId() {
+		String userId = "123a-456b";
+		final List<AddressDto> results = addressService.retrieveByUserId(userId);
+		assertEquals(dtoAddresses, results);
+
+		verify(addressRepository).findByUserId(userId);
+	}
+
+	private Address createAddress(final String id) {
+		final Address address = new Address();
+		address.setId(id);
+		return address;
+	}
+
+	private AddressDto createAddressDto(final String id) {
+		final AddressDto address = new AddressDto();
+		address.setId(id);
+		return address;
 	}
 
 }
